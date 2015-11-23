310673835915e79a49ca540b720199d76d76bb8f
tom <borkowskitomasz215@gmail.com>
2015-11-23 20:27:37 +0100

    Updated project structure and added some new libraries.

--- src/main/java/pl/edu/agh/iisg/to/Main.java
+++ src/main/java/pl/edu/agh/iisg/to/Main.java
@@ -1,7 +1,13 @@
 package pl.edu.agh.iisg.to;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 /**
  * Created by tom on 23.11.15.
  */
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.info("Main started");
+    }
 }
